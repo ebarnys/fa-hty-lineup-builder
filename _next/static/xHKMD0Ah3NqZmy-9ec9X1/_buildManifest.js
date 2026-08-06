604d1b77c573e8c42138d85ed0c936d38063a4e4
ebarnys <thomasbarnysneweak@gmail.com>
2026-08-06 14:48:07 +0200

    redeploy 1786020487

---
+++ _next/static/xHKMD0Ah3NqZmy-9ec9X1/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/fa-hty-lineup-builder//_next/:path+",
+        "destination": "/fa-hty-lineup-builder/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
